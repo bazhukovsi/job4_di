813d50f44546d0be00985eb0aac8d2b466d0b804
bazhukovsi <bazhukovsi@yandex.ru>
2023-01-19 19:37:18 +0300

    1. Spring DI. Запуск [#2091 #382946]

---
+++ src/main/java/job/di/homework/SpringDI.java
@@ -0,0 +1,15 @@
+package job.di.homework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.askStr("Введите ваш возраст: ");
+
+    }
+}
